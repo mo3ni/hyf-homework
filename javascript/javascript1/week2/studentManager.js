d5fe892d37c0316943264a36fbf134fa3e6815f7
Mohseni <mohseni.mr93@gmail.com>
2019-05-31 21:50:01 +0200

    done

--- javascript/javascript1/week2/studentManager.js
+++ javascript/javascript1/week2/studentManager.js
@@ -1,25 +1,45 @@
-//..... Student Manager......
+//............. Student Manager........
 const class07Students = [];
-class07Students.push('Ali');
-class07Students.push('Ali');
-class07Students.push('Ali');
-class07Students.push('Ali');
-class07Students.push('Ali');
-class07Students.push('Ali');
-class07Students.push('Ali');
-class07Students.push('Ali');
-
-class07Students.length = 6;
-
-//console.log(class07Students);
 
 function addStudentToClass(studentName) {
-    // You write code here
-
     
+    let massage = '';
+     if (getNumberOfStudents() > 6) {
+         massage =' More than 6 person is not allowed ';
+     } 
+     else if (studentName === "queen") {
+        class07Students.push("queen");
+        massage =' Seat for queen is always avialable ';
+    }
+     else if (studentName === "") {
+         massage = ' not empty value is allowed ! ';
+    } 
+    else if (duplicateValue(studentName)) {
+        massage = ' There is the same name in Stock !! ';
+    } 
+    else {
+        class07Students.push(studentName);
+        massage = studentName + ' Added to class !';
+      
+    }
+    return massage;
 }
-/*
 function getNumberOfStudents() {
-    // You write code here
+    return class07Students.length;
 }
-*/
+
+function duplicateValue(studentName) {
+    for (let i = 0; i < getNumberOfStudents(); i++) {
+        if (class07Students[i] === studentName) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+console.log(addStudentToClass("Benjamin"));
+console.log(addStudentToClass("Rasmus"));
+console.log(addStudentToClass("Rasmus"));
+console.log(addStudentToClass(""));
+
