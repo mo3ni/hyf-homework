4d1182376f348d614d5201d64debc388b15c345b
Mohseni <mohseni.mr93@gmail.com>
2019-06-01 17:40:45 +0200

    Need to work more on thisgit add candyHelper.js

---
+++ javascript/javascript1/week2/candyHelper.js
@@ -0,0 +1,74 @@
+// candy Helper 
+// note: >>> did not work on this field just copied from other person
+
+/*
+const boughtCandyPrices =[];
+
+function addCandy( candyType, weight){
+
+}
+*/
+//amount of money that customer want to spend for buying deliciouse candys
+const amountToSpend = Math.floor((Math.random() * 90) + 100);
+
+//candy class
+class Candy {
+    constructor(candyType, pricePerGram) {
+        this.candyType = candyType;
+        this.pricePerGram = pricePerGram;
+    }
+    addCandy(weight) {
+        return this.pricePerGram * weight;
+    }
+};
+
+//candy table is store candy options
+const candyTable = [
+    new Candy("Sweet", 0.5),
+    new Candy("Chocolate", 0.7),
+    new Candy("Toffee", 1.1),
+    new Candy("Chewing-gum", 0.03)
+]
+
+//check if we have more buget for buy more candys
+function canBuyMoreCandy(total) {
+    if (amountToSpend > total) {
+        console.log("You can buy more, so please do!");
+        return true;
+    } else {
+        console.log("Enough candy for you!");
+        return false;
+    }
+}
+
+//show basket
+function showBasket() {
+    for (const candyT of candyTable) {
+        let weightTotal = 0;
+        for (const candy of boughtCandy) {
+            if (candy.type === candyT.candyType) {
+                weightTotal += candy.weight;
+            };
+        }
+        console.log(`you have ${candyT.candyType} and total weight of it is ${weightTotal}`);
+    }
+}
+
+//run program
+const boughtCandy = [];
+total = 0;
+console.log(`You have ${amountToSpend} amount to spend`);
+while (canBuyMoreCandy(total)) {
+    let candyType = Math.floor(Math.random() * 4);
+    let weight = Math.random() * 10;
+
+    boughtCandy.push({
+        type: candyTable[candyType].candyType,
+        weight: weight
+    });
+
+    total += candyTable[candyType].addCandy(weight);
+}
+
+console.log(`In your basket you have: `)
+showBasket();
